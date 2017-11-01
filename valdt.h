bf7d727584fa271f09b81fc283aac244fc4b4269
Collin Clark <crclark96@gmail.com>
2017-11-01 12:55:00 -0400

    remove strlens from valdt.h

--- valdt.h
+++ valdt.h
@@ -1,12 +1,12 @@
 #ifndef VALDT_H
 #define VALDT_H
 
-int validate_user(char* user, char* pass, int len_u, int len_p);
+int validate_user(char* user, char* pass);
 /*
   validate that a user and password match */
 
 
-int validate_message(int message_id, char* pass, int len_p);
+int validate_message(int message_id, char* pass);
 /*
   validate that a passphrase for a given message id hashes correctly */
 
