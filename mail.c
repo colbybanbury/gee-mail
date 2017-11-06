b851577ef7023832bdebfacb7fa632e55754b1f5
colbybanbury <cbanbury@udel.edu>
2017-11-06 11:36:01 -0500

    changed all gets() to fgets()

--- mail.c
+++ mail.c
@@ -3,45 +3,31 @@
 #include "encr.h"
 #include "valdt.h"
 
-int main(void){
-  char* name, pass, sel, message, passphrase, destination;
-  init_db(0);
-  printf("Press s to sign in. Press r to register.\n");
-  gets(sel);
-  if(sel=='r'){
-    name = reg();
-  }else{
-    name = sign_in();
+char*reg();
+char* sign_in();
+
+char* reg(){
+  char username[50];
+  char password[50];
+  printf("enter a new username:");
+  fgets(username, 50, stdin);
+  while(check_user(username)){
+      printf("that username is already in use\n");
+      printf("enter a new username or type s to sign into an existing account:");
+      fgets(username, 50, stdin);
+      if(username == 's'){
+        return sign_in();
+      }
   }
+  printf("enter a new password:");
+  fgets(password, 50, stdin);
   
-  disp_messages();
-  printf("r to read, w to write\n");
-  gets(sel);
-  if(sel == 'r'){
-    printf("enter a message ID\n");
-    int id;
-    gets(id);
-    gets(passphrase);
-    if(validate(id, passphrase)){
-      message = decode(id, passphrase);
-      printf("%s\n",message);
-    }else{
-      printf("invalid passphrase\n");
-    }
-  }else{
-    gets(destination);
-    while(!check_user(destination)){
-      printf("no matching username\n");
-      gets(destination);
-    }
-    gets(message);
-    gets(passphrase);
-    message = encrypt(message,passphrase);
-    passphrase = hash(passphrase);
-    send_message(message,passphrase);
+  if(register_user(username, hash(password)) == 0)
+  {
+    printf("Successfully registered. Welcome!");
   }
-    
-  return 0;
+  memset(password, '0', 50);
+  return username;
 }
 
 char* sign_in(){
@@ -68,26 +54,44 @@ char* sign_in(){
   return username;
 }
 
-char* reg(){
-  char username[50];
-  char password[50];
-  printf("enter a new username:");
-  fgets(username, 50, stdin);
-  while(check_user(username)){
-      printf("that username is already in use\n");
-      printf("enter a new username or type s to sign into an existing account:");
-      fgets(username, 50, stdin);
-      if(username == 's'){
-        return sign_in();
-      }
+
+int main(void){
+  char* name, pass, sel, message, passphrase, destination;
+  init_db(0);
+  printf("Press s to sign in. Press r to register.\n");
+  fgets(sel, 1, stdin);
+  if(sel=='r'){
+    name = reg();
+  }else{
+    name = sign_in();
   }
-  printf("enter a new password:");
-  fgets(password, 50, stdin);
   
-  if(register_user(username, hash(password)) == 0)
-  {
-    printf("Successfully registered. Welcome!");
+  disp_messages();
+  printf("r to read, w to write\n");
+  fgets(sel, 1, stdin);
+  if(sel == 'r'){
+    printf("enter a message ID\n");
+    int id;
+    fgets(id, 20, stdin);
+    fgets(passphrase, 20, stdin);
+    if(validate(id, passphrase)){
+      message = decode(id, passphrase);
+      printf("%s\n",message);
+    }else{
+      printf("invalid passphrase\n");
+    }
+  }else{
+    fgets(destination, 20, stdin);
+    while(!check_user(destination)){
+      printf("no matching username\n");
+      fgets(destination, 20, stdin);
+    }
+    fgets(message, 140, stdin);
+    fgets(passphrase, 20, stdin);
+    message = encrypt(message,passphrase);
+    passphrase = hash(passphrase);
+    send_message(name, destination, message, passphrase);
   }
-  memset(password, '0', 50);
-  return username;
+    
+  return 0;
 }
